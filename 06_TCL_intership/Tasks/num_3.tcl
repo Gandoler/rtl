f9abf44ed448aa709c058365a0c9cff7f453228d
glebfrolov <gleb.frolov.04@bk.ru>
2025-07-15 15:41:25 +0300

    FEAT (TCL_internship) : решеиние номера 3

---
+++ 06_TCL_intership/Tasks/num_3.tcl
@@ -0,0 +1,22 @@
+set project_path "C:/Users/glkru/demoproject/demoproject.xpr"
+
+if {[file exists $project_path]} {
+    open_project $project_path
+    start_gui
+
+
+
+    set_property top demo_wrapper_nexys_a7 [current_fileset]
+
+
+    reset_run   synth_1
+    launch_runs synth_1 -jobs 4
+    wait_on_run synth_1
+    reset_run   impl_1
+    launch_runs impl_1  -jobs 4
+    wait_on_run impl_1
+
+
+    open_run impl_1
+    display_report utilization_report.txt
+}
