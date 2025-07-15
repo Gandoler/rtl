d7ffc4fac56d0f826cd677cdb40c4a687c453a8f
glebfrolov <gleb.frolov.04@bk.ru>
2025-07-15 13:45:27 +0300

    FEAT (TCL_intership) : добавил Tcl первого номера и убрал повторный запуск gui у нулевого

--- 06_TCL_intership/Tasks/num_0.tcl
+++ 06_TCL_intership/Tasks/num_0.tcl
@@ -2,7 +2,6 @@ set project_path "C:/Users/glkru/demoproject/demoproject.xpr"
 
 if {[file exists $project_path]} {
     open_project $project_path
-    start_gui
 
 
 set_property top tb_demo [get_filesets sim_1]
---
+++ 06_TCL_intership/Tasks/num_1.tcl
@@ -0,0 +1,9 @@
+set project_path "C:/Users/glkru/demoproject/demoproject.xpr"
+
+if {[file exists $project_path]} {
+    open_project $project_path
+    start_gui
+
+
+
+}
