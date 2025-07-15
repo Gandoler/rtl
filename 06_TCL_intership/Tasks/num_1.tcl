5f07b0b58c817adeacae3b038eb4d8b8e237c22d
glebfrolov <gleb.frolov.04@bk.ru>
2025-07-15 14:09:28 +0300

    Fix(TCl_internship) : додлела скрипт для номера 1

--- 06_TCL_intership/Tasks/num_1.tcl
+++ 06_TCL_intership/Tasks/num_1.tcl
@@ -4,6 +4,13 @@ if {[file exists $project_path]} {
     open_project $project_path
     start_gui
 
+     set_property top demo_wrapper_nexys_a7 [current_fileset]
+
+     update_compile_order -fileset sources_1
+
+     synth_design -rtl
+     show_schematic  -name "The Grates scheme"  [get_cells demo_wrapper_nexys_a7]
+
 
 
 }
