8b5712d974926ec63e6a83b5dfbb26cdafda7d8d
glebfrolov <gleb.frolov.04@bk.ru>
2025-07-15 13:41:20 +0300

    FEAT (TCL_intership ) : Добавил решение нулевого номера, которое запускает уже существующий проект

---
+++ 06_TCL_intership/Tasks/num_1.tcl
@@ -0,0 +1,17 @@
+set project_path "C:/Users/glkru/demoproject/demoproject.xpr"
+
+if {[file exists $project_path]} {
+    open_project $project_path
+    start_gui
+
+
+set_property top tb_demo [get_filesets sim_1]
+set_property top_lib xil_defaultlib [get_filesets sim_1]
+update_compile_order -fileset sim_1
+update_compile_order -fileset sources_1
+launch_simulation -mode behavioral 
+run 100 us
+start_gui
+
+
+}
