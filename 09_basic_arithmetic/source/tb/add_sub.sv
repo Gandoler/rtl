0bc3debd6f6b88158eb38dc82106ca8fe1dbc867
glebfrolov <gleb.frolov.04@bk.ru>
2025-07-21 11:00:14 +0300

    Fix(add_sub): Исправление синтаксических ошибок и улучшение кода модуля
    Исправлены следующие проблемы в модуле add_sub:
    - Заменен устаревший тип 'reg' на 'logic'
    - Удалены неиспользуемые переменные a_1, a_2, b_2, a_3, b_3

--- 09_basic_arithmetic/source/tb/add_sub.sv
+++ 09_basic_arithmetic/source/tb/add_sub.sv
@@ -1,6 +1,15 @@
 module add_sub;
 
-  reg [4:0] a_1, a_2, b_2, a_3, b_3;
-  reg [1:0] b_2;
-  reg [31:0] 
-  
+  logic [4:0] sum1, sum2, diff1, diff2;
+
+
+always_comb begin
+  sum1  = 5'd14 +  2'd2;
+  sum2  = 5'd20 + 5'd15;
+  diff1 = 5'd15 + ~(5'd5) + 1;
+  diff2 = 5'd15 + ~(5'd18) + 1;
+end
+
+
+
+endmodule
