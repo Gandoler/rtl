7e9d0f45a8dfcb686c93e86eb82f9c4e58805a77
glebfrolov <gleb.frolov.04@bk.ru>
2025-07-22 12:58:28 +0300

    FIX (09_basic_arithmetic/source/fpu_add) :   1) вынес из модуля typedef структуры   2) создал сдвиговые регистры для переноса результата

--- 09_basic_arithmetic/source/fpu/floating_point_adder.sv
+++ 09_basic_arithmetic/source/fpu/floating_point_adder.sv
@@ -1,3 +1,17 @@
+ typedef enum logic[1:0]{
+    OK  = 2'b00,
+    NAN = 2'b01,
+    INF = 2'b10,
+    NUL = 2'b11
+  } states;
+
+  typedef struct {
+    logic        sign;
+    logic [7:0]  exp;
+    logic [23:0] mant;
+  } float_point_num;
+
+
 module floating_point_adder (
     input               clk,
     input               rst,
@@ -10,29 +24,17 @@ module floating_point_adder (
     output logic [1:0]  state,
     output logic        res_vld
 );
-  enum logic[1:0]{
-    OK  = 2'b00,
-    NAN = 2'b01,
-    INF = 2'b10,
-    NUL = 2'b11
-  } states;
 
-  typedef struct {
-    logic        sign;
-    logic [7:0]  exp;
-    logic [23:0] mant
-  } float_point_num;
-
-  //сдвиговые регистры
-  float_point_num [5:0] a, b;
+  //shift ref
+  float_point_num  a_num [0:5], b_num [0:5];
   logic           [5:0] a_vld;
-
+  states          [5:0] states_res;
 
    always_ff @( posedge clk ) begin // fetch
     if (rst) begin
       a.sign <= 'b0;
       a.exp  <= 'b0;
-      a.mant <= 'b0
+      a.mant <= 'b0;
 
       b.sign <= 'b0;
       b.exp  <= 'b0;
@@ -40,7 +42,7 @@ module floating_point_adder (
     end else if(arg_vld) begin
       a.sign = a[31];
       a.exp  = a[30:23];
-      a.mant = a[22:0]
+      a.mant = a[22:0];
 
       b.sign = b[31];
       b.exp  = b[30:23];
