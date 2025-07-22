2065191240a479a15a1946927b0a885dae09e233
glebfrolov <gleb.frolov.04@bk.ru>
2025-07-22 13:59:08 +0300

    FEAT (09_basic_arithmetic/source/fpu_add) : добавил две конвейерных регистра для потока fp-чисел

--- 09_basic_arithmetic/source/fpu/floating_point_adder.sv
+++ 09_basic_arithmetic/source/fpu/floating_point_adder.sv
@@ -22,30 +22,59 @@ module floating_point_adder (
     output logic        res_vld
 );
 
+  logic           en;
+  float_point_num pipelined_input1;
+  float_point_num pipelined_input2;
+  float_point_num pipelined_num1 [0 : 6-1];
+  float_point_num pipelined_num2 [0 : 6-1];
+
+  shift_reg_for_struct shift_reg_for_struct_1#(
+  STAGES = 6;
+)(
+  .clk(clk),
+  .rst(rst),
+  .en(en),
+  .in_data(pipelined_input1),
+  .out_data(pipelined_num1)
+);
+
+shift_reg_for_struct shift_reg_for_struct_2#(
+  STAGES = 6;
+)(
+  .clk(clk),
+  .rst(rst),
+  .en(en),
+  .in_data(pipelined_input1),
+  .out_data(pipelined_num2)
+);
 
 
    always_ff @( posedge clk ) begin // fetch
     if (rst) begin
-      a.sign <= 'b0;
-      a.exp  <= 'b0;
-      a.mant <= 'b0;
+      pipelined_input1.sign <= 'b0;
+      pipelined_input1.exp  <= 'b0;
+      pipelined_input1.mant <= 'b0;
 
-      b.sign <= 'b0;
-      b.exp  <= 'b0;
-      b.mant <= 'b0;
+      pipelined_input2.sign <= 'b0;
+      pipelined_input2.exp  <= 'b0;
+      pipelined_input2.mant <= 'b0;
     end else if(arg_vld) begin
-      a.sign = a[31];
-      a.exp  = a[30:23];
-      a.mant = a[22:0];
+      pipelined_input1.sign = a[31];
+      pipelined_input1.exp = a[30:23];
+      pipelined_input1.mant = a[22:0];
 
-      b.sign = b[31];
-      b.exp  = b[30:23];
-      b.mant = b[22:0];
+      pipelined_input2.sign = b[31];
+      pipelined_input2.exp = b[30:23];
+      pipelined_input2.mant = b[22:0];
     end
-
   end
 
-
+  always_ff @( posedge clk ) begin
+    if(rst)
+      en <= 'b0;
+    else if(!((&pipelined_input1.exp) == 'b1))
+      en <= 'b1;
+  end
 
   always_comb begin
 
