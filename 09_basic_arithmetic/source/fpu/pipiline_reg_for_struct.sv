ae7bcef942abfffc67b0bbda2c36163f771d876f
glebfrolov <gleb.frolov.04@bk.ru>
2025-07-23 10:41:27 +0300

    FIX (09_basic_arithmetic/source/fpu_add) :  исправил неправильный переброс в сдвиговом регистре

--- 09_basic_arithmetic/source/fpu/pipiline_reg_for_struct.sv
+++ 09_basic_arithmetic/source/fpu/pipiline_reg_for_struct.sv
@@ -1,33 +1,32 @@
 `include "struct_types.sv"
 import float_struct::*;
 
-
-module shift_reg_for_struct#(
+module shift_reg_for_struct #(
   parameter STAGES = 6
 )(
   input                  clk,
   input                  rst,
   input  logic           en,
   input  float_point_num in_data,
-
-  output float_point_num out_data [0 : STAGES-1]
+  output float_point_num out_data [0:STAGES-1]
 );
 
+float_point_num reg_data [0:STAGES-1];
 
-
-
-  always_ff @(posedge clk) begin
-    if(rst) begin
-        for(int i=0; i < STAGES; i++)
-          out_data[i] <='b0;
-    end
-    else if(en) begin
-      out_data[0] <= in_data
-      for(int i=1; i < STAGES; i++) begin
-        out_data[i] <= out_data[i-1];
-      end
-    end
-
-
+always_ff @(posedge clk) begin
+  if (rst) begin
+    foreach (reg_data[i])
+      reg_data[i] <= '{sign:1'b0, exp:8'b0, mant:23'b0};
   end
+  else if (en) begin
+    reg_data[0] <= in_data; 
+    
+    for (int i = 1; i < STAGES; i++)
+      reg_data[i] <= reg_data[i-1];
+  end
+end
+
+// Присвоение выходного сигнала
+assign out_data = reg_data;
+
 endmodule
