ed032d867bcdd173c120aec1598494ef7d7ee6bf
glkru <gl.krutoi@mail.ru>
2025-07-24 08:40:00 +0300

    FIX (09_basic_arithmetic/source/tb) : поменял назние файла(по ошибке назвал его последовательным )

--- 09_basic_arithmetic/source/comb_fp_summator.sv
+++ 09_basic_arithmetic/source/comb_fp_summator.sv
@@ -17,13 +17,15 @@ module sequence_fp_summator (
 );
 
   float_point_num a_l, b_l;
-  logic           num_status;
 
   always_comb begin : fetch
     a_l = '{sign : a_i.sign, exp : a_i.exp, mant : ({1'b1, a_i.mant})};
 
     b_l = '{sign : b_i.sign, exp : b_i.exp, mant : ({1'b1, b_i.mant})};
   end
+
+
+  logic           num_status;
 
   always_comb begin : status
     if(((&a_i.exp) == 'b1) || ((&b_i.exp) == 'b1))
@@ -32,7 +34,13 @@ module sequence_fp_summator (
       num_status = 1'b1;
   end
 
-  always_comb begin :
+ logic
+
+  always_comb begin : get_exp_diff
+
+
+
+  end
 
 
 
