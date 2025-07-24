079ee07ae0f76fd2d6fbe4a333835903367daf73
glebfrolov <gleb.frolov.04@bk.ru>
2025-07-24 10:29:05 +0300

    FEAT (09_basic_arithmetic/source/fpu_reborn) : добавил пекейдж

--- 09_basic_arithmetic/source/FPU_reborn/fetch_stage.sv
+++ 09_basic_arithmetic/source/FPU_reborn/fetch_stage.sv
@@ -1,27 +1,31 @@
-module fetch_stage(`include "struct_types.sv"
+module fetch_stage import float_types_pkg::*;(
   input  logic           valid_i,
   input  float_point_num a_i,
   input  float_point_num b_i,
 
   output float_point_num a_o,
   output float_point_num b_o,
-  output logic [1:0]     num_status
-
+  output logic           num_status
 );
 
+always_comb begin
+  if (valid_i) begin
+    a_o     = '{sign: a_i.sign, exp: a_i.exp, mant : ({1'b1, a_i.mant})};
 
-always_comb begin :
-
-    if(arg_vld) begin
-      a_o.sign  <= a_i.sign;
-      a_o.exp   <= a_i.exp;
-      a_o.mant  <= {1'b1, a_i.mant };
-
-      b_o.sign  <= b_i.sign;
-      b_o.exp   <= b_i.exp;
-      b_o.mant  <= {1'b1, b_i.mant };
-    end
+    b_o     = '{sign: b_i.sign, exp: b_i.exp, mant : ({1'b1, b_i.mant})};
+  end else begin
+    a_o = a_i;
+    b_o = b_i;
+  end
 end
+
+always_comb begin
+ if(((&a_i.exp) == 1'b1) || ((&b_i.exp) == 1'b1))
+   num_status = 1'b0;
+ else
+    num_status = 1'b1;
+end
+
 
 
 endmodule
--- 09_basic_arithmetic/source/FPU_reborn/struct_types.sv
+++ 09_basic_arithmetic/source/FPU_reborn/struct_types.sv
@@ -1,13 +1,12 @@
-package struct_types;
-  typedef struct {
-    logic        sign;
-    logic [7:0]  exp;
-    logic [23:0] mant;
-  } float_point_num;
+package float_types_pkg;
+typedef struct packed {
+  logic        sign;
+  logic [7:0]  exp;
+  logic [23:0] mant;
+} float_point_num;
 
-  typedef enum logic[1:0]{
-    INF  = 00,
-    NAN  = 01,
-    GOOD = 11
-  } num_status;
+typedef enum logic{
+  INF_OR_NAN  = 1'b0,
+  GOOD = 1'b1
+} num_status;
 endpackage
