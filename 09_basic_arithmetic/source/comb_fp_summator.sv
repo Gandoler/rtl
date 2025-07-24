84f0cff05cb7b9868be013e31544e8df8b755119
glkru <gl.krutoi@mail.ru>
2025-07-24 09:30:04 +0300

    FEAT (09_basic_arithmetic/source/tb) : доделал модуль прохожу к исправлению синтаксических ошибок

--- 09_basic_arithmetic/source/comb_fp_summator.sv
+++ 09_basic_arithmetic/source/comb_fp_summator.sv
@@ -12,8 +12,8 @@ module sequence_fp_summator (
   input  logic           vld_i,
 
 
-  output logic           answer_status,
-  output float_point_num answer_0
+  output logic           answer_status_o,
+  output float_point_num answer_o
 );
 
   float_point_num a_l, b_l;
@@ -76,7 +76,46 @@ module sequence_fp_summator (
     end
   end
 
+  logic float_point_num answer;
 
+
+    logic denormilize;
+  always_comb begin : get_res
+    if(mant_sum[24]) begin
+      answer = '{sign : res_sign, exp : (a_l.exp + 1), mant : mant_sum[23:1]};
+      denormilize = 1'b0;
+    end else if (mant_sum[23]) begin
+      answer = '{sign : res_sign, exp : a_l.exp, mant : mant_sum[22:0]};
+      denormilize = 1'b0;
+    end else begin
+      answer = '{sign : 'b0, exp : 'b0, mant : 'b0};
+      denormilize = 1'b1;
+    end
+  end
+
+  logic found_1;
+
+  always_comb begin : denormilize_case
+    found_1 = 1'b0;
+    if(denormilize) begin
+      for(int i = 22; i >= 0; i--)begin
+        if(mant_sum[i] && (!found_1)) begin
+          a_l.exp  <= a_l.exp - (22 - i + 1);
+          mant_sum <= mant_sum << (22 - i);
+          found_1                <= 'b1;
+        end
+      end
+
+
+      if(found_1) begin
+        answer = '{sign : res_sign, exp : a_l.exp , mant : mant_sum[22:0]};
+      end else
+        answer.sign   <= 'b0;
+    end
+  end
+
+
+assign answer_o = '{sign : answer,sign, exp : answer.exp , mant : answer.mant};
 
 
 endmodule
