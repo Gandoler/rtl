513f60e2ab0d99bfdf72c70ce25fecf9819ad717
glkru <gl.krutoi@mail.ru>
2025-07-24 08:54:01 +0300

    FEAT (09_basic_arithmetic/source/tb) : добавил три "стадии" :
    1) получение разниц экспонент
    2) сдвиг
    3) сумму мантисс

--- 09_basic_arithmetic/source/comb_fp_summator.sv
+++ 09_basic_arithmetic/source/comb_fp_summator.sv
@@ -34,14 +34,47 @@ module sequence_fp_summator (
       num_status = 1'b1;
   end
 
- logic
+ logic       larger_exp;
+ logic [7:0] exp_dif;
 
   always_comb begin : get_exp_diff
-
-
-
+    larger_exp = a_l.exp > b_l.exp;
+    if(larger_exp)
+      exp_dif = a_l.exp - b_l.exp;
+    else
+      exp_dif = b_l.exp - a_l.exp;
   end
 
+
+  always_comb begin : mant_shift
+    if(larger_exp) begin
+      b_l.mant = b_l.mant >> exp;
+      b_l.exp  = a_l.exp;
+    end else begin
+      a_l.mant = a_l.mant >> exp;
+      a_l.exp  = b_l.exp;
+    end
+  end
+
+
+  logic [24:0] mant_sum;
+  logic        res_sign;
+
+
+  always_comb begin : mant_sum
+    if(a_l.sign = b_l.sign) begin
+      mant_sum = {1'b0, a_l.mant} + {1'b0, b_l.mant};
+      res_sign = a_l.sign;
+    end else begin
+      if(a_l.sign)begin
+        mant_sum = {1'b0, b_l.mant} - {1'b0, a_l.mant};
+        res_sign = a_l.mant > b_l.mant;
+      end else begin
+         mant_sum = {1'b0, a_l.mant} - {1'b0, b_l.mant};
+         res_sign = b_l.mant > a_l.mant;
+      end
+    end
+  end
 
 
 
