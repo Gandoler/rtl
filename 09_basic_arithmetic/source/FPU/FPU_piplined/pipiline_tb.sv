34ce70375fe48298868874b7ac11a17f4231ead2
glkru <gl.krutoi@mail.ru>
2025-07-28 12:43:55 +0300

    FIX (09_basic_arithmetic/source/pipilined_summator) : исправил ошибки тб

--- 09_basic_arithmetic/source/FPU/FPU_piplined/pipiline_tb.sv
+++ 09_basic_arithmetic/source/FPU/FPU_piplined/pipiline_tb.sv
@@ -1,7 +1,7 @@
 `timescale  1ns/1ps
 
 
-module pipiline_tb;
+module pipiline_tb import float_types_pkg::*;;
 
   reg clk;
   reg rst;
@@ -13,7 +13,7 @@ module pipiline_tb;
 
    pipilined_fp_summator DUT(
     .clk_i(clk),
-    .rst(rst_i),
+    .rst_i(rst),
 
     .a_i(a_i),
     .b_i(b_i),
@@ -22,17 +22,24 @@ module pipiline_tb;
     .num_status_o(answer_status_o),
     .answer_o(answer_o));
 
-    initial begin
+    initial 
+       clk = 0;
        always #10 clk = ~clk;
-       rst = 1;
-       #40;
-       vld_i =1'b1;
-       a_i = '{sign : 1'b0, exp :8'b0111_1110, mant : 23'b11000000000000000000000};  //0.875
-       b_i = '{sign : 1'b0, exp :8'b1000_0000, mant : 23'b00011001100110011001101};    // 2.2
-       #50;
-       //0	10000000	10001001100110011001101 --RES
-       res = '{sign : 1'b0, exp :8'b1000_0000, mant : 23'b10001001100110011001101}; // 3.075
-
+       initial begin
+         rst = 1;
+         #40;
+         vld_i =1'b1;
+         a_i = '{sign : 1'b0, exp :8'b0111_1110, mant : 23'b11000000000000000000000};  //0.875
+         b_i = '{sign : 1'b0, exp :8'b1000_0000, mant : 23'b00011001100110011001101};    // 2.2
+         #50;
+         //0	10000000	10001001100110011001101 --RES
+         res = '{sign : 1'b0, exp :8'b1000_0000, mant : 23'b10001001100110011001101}; // 3.075
+           if (answer_o.sign == res.sign && answer_o.exp == res.exp && answer_o.mant == res.mant) begin
+             $display("TEST PASSED: Results match!");
+           end else begin
+             $display("TEST FAILED: Results do not match!");
+           end
 
     end
+  
 endmodule
