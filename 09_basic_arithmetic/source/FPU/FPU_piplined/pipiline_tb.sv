7c585be77bb5bc9f2a92fe77d57dd40d0fc00b59
glkru <gl.krutoi@mail.ru>
2025-07-28 13:30:11 +0300

    FEAT(09_basic_arithmetic/source/pipilined_summator) : закончил тесты, все воркает

--- 09_basic_arithmetic/source/FPU/FPU_piplined/pipiline_tb.sv
+++ 09_basic_arithmetic/source/FPU/FPU_piplined/pipiline_tb.sv
@@ -22,7 +22,7 @@ module pipiline_tb import float_types_pkg::*;;
     .num_status_o(answer_status_o),
     .answer_o(answer_o));
 
-    initial 
+    initial
        clk = 0;
        always #5 clk = ~clk;
        initial begin
@@ -32,15 +32,78 @@ module pipiline_tb import float_types_pkg::*;;
          vld_i =1'b1;
          a_i = '{sign : 1'b0, exp :8'b0111_1110, mant : 23'b11000000000000000000000};  //0.875
          b_i = '{sign : 1'b0, exp :8'b1000_0000, mant : 23'b00011001100110011001101};    // 2.2
-         #40;
+         #10;
+
+         vld_i =1'b1;
+         a_i = '{sign : 1'b0, exp :8'b01111111, mant : 23'b00000000000000000000000};  // 1
+         b_i = '{sign : 1'b0, exp :8'b01111111, mant : 23'b00000000000000000000000};  // 1
+         #10
+         vld_i =1'b1;
+         a_i = '{sign : 1'b0, exp :8'b1000_0000, mant : 23'b00000000000000000000000};  // 2
+         b_i = '{sign : 1'b1, exp :8'b0111_1111, mant : 23'b00000000000000000000000};  // -1
+         #10
+         vld_i =1'b1;
+         a_i = '{sign : 1'b0, exp :8'b01111110, mant : 23'b10000000000000000000000};  // 0.75
+         b_i  = '{sign : 1'b0, exp :8'b01111101, mant : 23'b00000000000000000000000};  // 0.25
+         #10
+         vld_i =1'b1;
+         a_i = '{sign : 1'b0, exp :8'b10000001, mant : 23'b01110000000000000000000};  // 5.75
+         b_i = '{sign : 1'b1, exp :8'b10000001, mant : 23'b01110000000000000000000};  // -5.75
+
          //0	10000000	10001001100110011001101 --RES
          res = '{sign : 1'b0, exp :8'b1000_0000, mant : 23'b10001001100110011001101}; // 3.075
-           if (answer_o.sign == res.sign && answer_o.exp == res.exp && answer_o.mant == res.mant) begin
-             $display("TEST PASSED: Results match!");
-           end else begin
-             $display("TEST FAILED: Results do not match!");
-           end
+         if (answer_o.sign == res.sign && answer_o.exp == res.exp && answer_o.mant == res.mant) begin
+           $display("TEST PASSED: Results match!");
+         end else begin
+           $display("TEST FAILED: Results do not match!");
+         end
+         #10
 
+
+         res = '{sign : 1'b0, exp :8'b1000_0000, mant : 23'b00000000000000000000000}; // 2
+         if (answer_o.sign == res.sign && answer_o.exp == res.exp && answer_o.mant == res.mant) begin
+          $display("TEST PASSED: Results match!");
+         end else begin
+          $display("TEST FAILED: Results do not match!");
+         end
+         #10
+
+         res = '{sign : 1'b0, exp :8'b01111111, mant : 23'b00000000000000000000000}; // 1
+         if (answer_o.sign == res.sign && answer_o.exp == res.exp && answer_o.mant == res.mant) begin
+           $display("TEST PASSED: Results match!");
+         end else begin
+           $display("TEST FAILED: Results do not match!");
+         end
+         #10
+
+
+         res = '{sign : 1'b0, exp :8'b01111111, mant : 23'b00000000000000000000000}; // 1
+         if (answer_o.sign == res.sign && answer_o.exp == res.exp && answer_o.mant == res.mant) begin
+           $display("TEST PASSED: Results match!");
+         end else begin
+           $display("TEST FAILED: Results do not match!");
+         end
+
+
+         vld_i =1'b0;
+         #40
+         vld_i =1'b1;
+         a_i = '{sign : 1'b0, exp :8'b00000000, mant : 23'b00000000000000000000000}; // 0
+         b_i = '{sign : 1'b0, exp :8'b00000000, mant : 23'b00000000000000000000000}; // 0
+         #40
+
+
+          res = '{sign : 1'b0, exp :8'b00000000, mant : 23'b00000000000000000000000}; // 0
+         if (answer_status_o == ZERO_res) begin
+           $display("TEST PASSED: Results match!");
+         end else begin
+           $display("TEST FAILED: Results do not match!");
+         end
+
+
+
+
+      $finish;
     end
-  
+
 endmodule
