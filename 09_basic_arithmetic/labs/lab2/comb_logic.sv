cac09ce9834c7ad23cc97d54ad74aee2e1ff30d8
glebfrolov <gleb.frolov.04@bk.ru>
2025-07-29 16:41:32 +0300

    Feat(09_basic_arithmetic.labs/lab2) : имплементировал решение задачи и тб к ним :
    1) лог функция на лутах(тут надо не забывать о обратном порядке переменных)
    2) минус и плюс на carry4(минус тот же плюс просто с инверсией b )

--- 09_basic_arithmetic/labs/lab2/comb_logic.sv
+++ 09_basic_arithmetic/labs/lab2/comb_logic.sv
@@ -7,23 +7,35 @@ module comb_logic (
   output logic [3:0] sum_o
 );
 
-  LUT3 #(.INIT(8'h1E) ) LUT3_0 (.O(lut_o[0]), .I0(a[0]), .I1(b[0]), .I2(c[0]));
-  LUT3 #(.INIT(8'h1E) ) LUT3_1 (.O(lut_o[1]), .I0(a[1]), .I1(b[1]), .I2(c[1]));
-  LUT3 #(.INIT(8'h1E) ) LUT3_2 (.O(lut_o[2]), .I0(a[2]), .I1(b[2]), .I2(c[2]));
-  LUT3 #(.INIT(8'h1E) ) LUT3_3 (.O(lut_o[3]), .I0(a[3]), .I1(b[3]), .I2(c[3]));
+  LUT3 #(.INIT(8'h1E) ) LUT3_0 (.O(lut_o[0]), .I0(c[0]), .I1(b[0]), .I2(a[0]));
+  LUT3 #(.INIT(8'h1E) ) LUT3_1 (.O(lut_o[1]), .I0(c[1]), .I1(b[1]), .I2(a[1]));
+  LUT3 #(.INIT(8'h1E) ) LUT3_2 (.O(lut_o[2]), .I0(c[2]), .I1(b[2]), .I2(a[2]));
+  LUT3 #(.INIT(8'h1E) ) LUT3_3 (.O(lut_o[3]), .I0(c[3]), .I1(b[3]), .I2(a[3]));
 
-  logic [3:0] S, DI, CO;
+  logic [3:0] S, DI, S2, DI2;
 
    assign S = a ^ b;
    assign DI = a & b;
 
 
-  CARRY4 carry4_inst (
+  CARRY4 carry4_sum (
     .CI('b0),
     .DI(DI),
     .S(S),
     .CO(),
-    .O(Sum)
+    .O(sum_o)
+  );
+
+  assign S2 = a ^ ~b;
+  assign DI2 = a & ~b;
+
+  CARRY4 carry4_minus (
+    .CI('b0),
+    .CYINIT(1'b1),
+    .DI(DI2),
+    .S(S2),
+    .CO(),
+    .O(minus_o)
   );
 
 endmodule
--- 09_basic_arithmetic/labs/lab2/lab_2_tb.sv
+++ 09_basic_arithmetic/labs/lab2/lab_2_tb.sv
@@ -1,19 +1,54 @@
-module lab_2_tb ();
-  logic [3:0] a,
-  logic [3:0] b,
-  logic [3:0] c,
-  logic [3:0] lut_o,
-  logic [3:0] minus_o,
-  logic [3:0] sum_o
+`timescale 1ns / 1ps
 
-  comb_logic (
-  .a(a),
-  .b(b),
-  .c(c),
-  .lut_o(lut_o),
-  .minus_o(minus_o),
-  .sum_o(sum_o)
-);
+module lab_2_tb;
 
+  logic [3:0] a, b, c;
+  logic [3:0] lut_o;
+  logic [3:0] minus_o;
+  logic [3:0] sum_o;
+
+  logic [3:0] expected_lut, expected_minus, expected_sum;
+
+  comb_logic dut (
+    .a(a),
+    .b(b),
+    .c(c),
+    .lut_o(lut_o),
+    .minus_o(minus_o),
+    .sum_o(sum_o)
+  );
+
+  task check_outputs;
+    begin
+      expected_lut   = (a ^ (b | c));
+      expected_minus = a - b;
+      expected_sum   = a + b;
+
+      $display("a=%b b=%b c=%b | lut_o=%b sum_o=%b minus_o=%b", a, b, c, lut_o, sum_o, minus_o);
+
+      if (lut_o !== expected_lut)
+        $error("LUT mismatch: expected %b, got %b", expected_lut, lut_o);
+
+      if (minus_o !== expected_minus)
+        $error("MINUS mismatch: expected %b, got %b", expected_minus, minus_o);
+
+      if (sum_o !== expected_sum)
+        $error("SUM mismatch: expected %b, got %b", expected_sum, sum_o);
+    end
+  endtask
+
+  initial begin
+
+    a = 4'b0000; b = 4'b0000; c = 4'b0000; #10; check_outputs();
+    a = 4'b1010; b = 4'b0101; c = 4'b1111; #10; check_outputs();
+    a = 4'b1111; b = 4'b1111; c = 4'b0000; #10; check_outputs();
+    a = 4'b1001; b = 4'b0110; c = 4'b0011; #10; check_outputs();
+    a = 4'b0011; b = 4'b0001; c = 4'b1010; #10; check_outputs();
+    a = 4'b1100; b = 4'b0011; c = 4'b0101; #10; check_outputs();
+    a = 4'b0001; b = 4'b0001; c = 4'b0001; #10; check_outputs();
+
+    $display("All test cases completed.");
+    $stop;
+  end
 
 endmodule
