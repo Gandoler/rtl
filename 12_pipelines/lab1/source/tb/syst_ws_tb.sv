b69df991eb33f0f827ef2e232e628780b2b10367
Gleb <gleb.frolob.o4@bk.ru>
2025-09-08 14:42:54 +0300

    FEAT(glebFrolov.pipelines/lab1/source/tb/syst_ws): тест работает!!!!!!!!!!!

--- 12_pipelines/lab1/source/tb/syst_ws_tb.sv
+++ 12_pipelines/lab1/source/tb/syst_ws_tb.sv
@@ -56,7 +56,7 @@ syst_ws syst_ws1
 
   mailbox#(packet_in) gen2drv = new();
   mailbox#(packet_in) in_mbx  = new();
-  mailbox#(packet_out) out_mbx_01 = new();
+  mailbox#(packet_out) out_mbx = new();
 
 
 //---------------------------------
@@ -84,9 +84,9 @@ syst_ws syst_ws1
       input int size_max   = 10
   );
     packet_in p;
-    p.x1_i = $urandom_range(0, 2**WIDTH_X-1);
-    p.x2_i = $urandom_range(0, 2**WIDTH_X-1);
-    p.x3_i = $urandom_range(0, 2**WIDTH_X-1);
+    p.x1_i = $urandom_range(0,5);// 2**WIDTH_X-1
+    p.x2_i = $urandom_range(0, 5);
+    p.x3_i = $urandom_range(0, 5);
     gen2drv.put(p);
 
   endtask
@@ -111,97 +111,108 @@ syst_ws syst_ws1
 
 
 
-  task drive_master(packet_in p);
+  task drive_master(input packet_in p ,input int set_delay=2);
         x1_i  <= p.x1_i;
         x2_i  <= p.x2_i;
         x3_i  <= p.x3_i;
-        @(posedge clk_i);
+        repeat(set_delay)@(posedge clk_i);
   endtask
 
 
-  task do_master_drive();
+  task do_master_drive(input int set_delay=2);
         packet_in p;
         reset_master();
         @(posedge clk_i);
         forever begin
             gen2drv.get(p);
-            drive_master(p);
+            drive_master(p,set_delay);
         end
   endtask
 
 
-  task monitor_master();
+  task monitor_master(input int set_delay=2);
         packet_in p;
-        @(posedge clk_i);
-            p.x1_i  = x1_i;
-            p.x2_i  = x2_i;
-            p.x3_i  = x3_i;
-            in_mbx.put(p);
+          p.x1_i  = x1_i;
+          p.x2_i  = x2_i;
+          p.x3_i  = x3_i;
+          in_mbx.put(p);
   endtask
 
 
-  task do_master_monitor();
+  task do_master_monitor(input int set_delay=2);
         wait(~rst_i);
+        repeat(set_delay)@(posedge clk_i);
         forever begin
-            monitor_master();
+            monitor_master(set_delay);
+            repeat(set_delay)@(posedge clk_i);
         end
   endtask
 
   // Master
-  task master(input
-      int gen_pkt_amount = 100
+  task master(
+      input int gen_pkt_amount = 100,
+      input int set_delay=2
   );
       fork
           do_master_gen(gen_pkt_amount);
-          do_master_drive();
-          do_master_monitor();
+          do_master_drive(set_delay);
+          do_master_monitor(set_delay);
       join
   endtask
 
 
   // Slave
-  task monitor_slave();
+  task monitor_slave(input int set_delay=2);
         packet_out p;
+        p.y1_o  = y1_o;
         @(posedge clk_i);
-          p.y1_o  = y1_o;
-          p.y2_o  = y1_o;
-          out_mbx.put(p);
+        p.y2_o  = y2_o;
+        out_mbx.put(p);
   endtask
 
-  task do_slave_monitor();
+  task do_slave_monitor(input int set_delay=2);
         wait(~rst_i);
+        if (set_delay !=3)
+          $display("delay=%0d",set_delay);
+          
+        repeat(set_delay + 2)@(posedge clk_i);
         forever begin
-            monitor_slave();
+            monitor_slave(set_delay);
+            repeat(set_delay-1)@(posedge clk_i);
         end
   endtask
 
   // Slave
-  task slave();
+  task slave(input int set_delay=2);
       fork
-          do_slave_monitor();
+          do_slave_monitor(set_delay);
       join
   endtask
 
 
 
   // Проверка
-  task check_01(packet_in in_prev, packet_out out_prev, packet_out out);
-      static logic [WIDTH_Y:0] y1_expected = in_prev.x1_i*W11 + in_prev.x2_i*W12 + in_prev.x3_i*W13;
-      static logic [WIDTH_Y:0] y2_expected = in_prev.x1_i*W11 + in_prev.x2_i*W12 + in_prev.x3_i*W13;
+  task check_01(packet_in in_prev, packet_out out);
+      logic [WIDTH_Y:0] y1_expected;
+      logic [WIDTH_Y:0] y2_expected;
 
-      if(out_prev.y1_o !==  y1_expected)
-        $error("[%0t] ERROR: input_data: x1=%0d,x2=%0d,x3=%0d expected: y1=%0d, got: y1=%0d",
-            $time, in_prev.x1_input, in_prev.x2_input, in_prev.x3_input, y1_expected, out_prev.y1_o);
-      else
-        $display("[%0t] OK:  input_data: x1=%0d,x2=%0d,x3=%0d, res: y1=%0d",
-          $time,  in_prev.x1_input, in_prev.x2_input, in_prev.x3_input, out_prev.y1_o);
+      y1_expected = in_prev.x1_i * W11 + in_prev.x2_i * W12 + in_prev.x3_i * W13;
+      y2_expected = in_prev.x1_i * W21 + in_prev.x2_i * W22 + in_prev.x3_i * W23;
+
 
       if(out.y1_o !==  y1_expected)
         $error("[%0t] ERROR: input_data: x1=%0d,x2=%0d,x3=%0d expected: y1=%0d, got: y1=%0d",
-            $time, in_prev.x1_input, in_prev.x2_input, in_prev.x3_input, y1_expected, out.y1_o);
+            $time, in_prev.x1_i, in_prev.x2_i, in_prev.x3_i, y1_expected, out.y1_o);
       else
         $display("[%0t] OK:  input_data: x1=%0d,x2=%0d,x3=%0d, res: y1=%0d",
-          $time,  in_prev.x1_input, in_prev.x2_input, in_prev.x3_input, out.y1_o);
+          $time,  in_prev.x1_i, in_prev.x2_i, in_prev.x3_i, out.y1_o);
+
+      if(out.y2_o !==  y2_expected)
+        $error("[%0t] ERROR: input_data: x1=%0d,x2=%0d,x3=%0d expected: y2=%0d, got: y2=%0d\n",
+            $time, in_prev.x1_i, in_prev.x2_i, in_prev.x3_i, y2_expected, out.y2_o);
+      else
+        $display("[%0t] OK:  input_data: x1=%0d,x2=%0d,x3=%0d, res: y2=%0d\n",
+          $time,  in_prev.x1_i, in_prev.x2_i, in_prev.x3_i, out.y2_o);
 
 
   endtask
@@ -210,18 +221,18 @@ syst_ws syst_ws1
 
   task do_check(input int pkt_amount = 1);
       int cnt;
-      packet_in in_p_prev,in_p;
-      packet_out out_p_prev, out_p;
-      
-      in_mbx.get(in_p_prev);
-      out_mbx.get(out_p_prev);
+      packet_in in_p;
+      packet_out out_p;
+
+
       forever begin
           in_mbx.get(in_p);
           out_mbx.get(out_p);
-          check_01(in_p_prev, out_p_prev, out_p);
+          //$display("Input x1=%0d x2=%0d x3=%0d",in_p.x1_i, in_p.x2_i, in_p.x3_i);
+          //$display("Output y1=%0d y2=%0d",out_p.y1_o, out_p.y2_o);
+
+          check_01(in_p, out_p);
           cnt        = cnt + 1;
-          in_p_prev  = in_p;
-          out_p_prev = out_p;
           if( cnt == pkt_amount ) begin
               break;
           end
@@ -248,12 +259,13 @@ syst_ws syst_ws1
 
 
   task test(
-        input int gen_pkt_amount   = 100,   // количество пакетов
-        input int timeout_cycles   = 100000 // таймаут теста
+        input int gen_pkt_amount   = 100,    // количество пакетов
+        input int timeout_cycles   = 100000, // таймаут теста
+        input int set_delay        = 2
     );
         fork
-            master       (gen_pkt_amount);
-            slave        ();
+            master       (gen_pkt_amount, set_delay);
+            slave        (set_delay);
             error_checker(gen_pkt_amount);
             timeout      (timeout_cycles);
         join
@@ -265,7 +277,8 @@ syst_ws syst_ws1
         join_none
         test(
             .gen_pkt_amount (    100),
-            .timeout_cycles (1000000)
+            .timeout_cycles (1000000),
+            .set_delay(3)
         );
     end
 
